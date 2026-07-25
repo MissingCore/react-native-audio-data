f74543dd44061b84de617ee2a87e4abc6f74ca20
cyanChill <83375816+cyanChill@users.noreply.github.com>
2026-07-25 15:39:20 -0400

    chore: upgrade Nitro version
    - There doesn't seem to be any uses of `bitint` in here.
    - The only thing we need to change is the `cpp-adapter.cpp` file.

--- android/src/main/cpp/cpp-adapter.cpp
+++ android/src/main/cpp/cpp-adapter.cpp
@@ -1,6 +1,9 @@
 #include <jni.h>
+#include <fbjni/fbjni.h>
 #include "audiodataOnLoad.hpp"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::audiodata::initialize(vm);
+  return facebook::jni::initialize(vm, []() {
+    margelo::nitro::audiodata::registerAllNatives();
+  });
 }
